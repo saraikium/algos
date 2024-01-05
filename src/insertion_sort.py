b44c79799e4438db43382ee22b983c2d261d43bb
saraikium <adnank122@gmail.com>
2024-01-05 06:17:20 +0500

    Insertion sort

---
+++ src/insertion_sort.py
@@ -0,0 +1,20 @@
+def insertion_sort(input: list[int]) -> list[int]:
+    for i in range(1, len(input)):
+        key = input[i]
+
+        j = i - 1
+        # Move elements from i - 1 to 0 one position to the right,
+        # until we find the right place for the key to insert
+        # at which point we insert the key in that place.
+        # thus the sub array A[0: i -1] always remains sorted
+        while j >= 0 and input[j] > key:
+            input[j + 1] = input[j]
+            j = j - 1
+        else:
+            input[j + 1] = key
+
+    return input
+
+
+arr = [589, 27, 9273, 189, 174, 187, 224, 1]
+print(insertion_sort(arr))
